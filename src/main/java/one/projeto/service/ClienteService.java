816b4bae355525ece217412973860234ed86433c
MarcoNaya <marco.gntz@gmail.com>
2024-07-28 17:10:05 -0300

    first commit

---
+++ src/main/java/one/projeto/service/ClienteService.java
@@ -0,0 +1,17 @@
+package one.projeto.service;
+
+import one.projeto.model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
